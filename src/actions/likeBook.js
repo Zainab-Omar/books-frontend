4b98bde30a9f887d90bdb418e7d0ff549f0eb86b
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-04-22 16:03:23 -0500

    create likeBook action

---
+++ src/actions/likeBook.js
@@ -0,0 +1,24 @@
+const addLikesToBook = (book) => {
+    return {
+      type: 'LIKE_BOOK',
+      perfume
+    }
+}
+
+
+export const likeBook = (book) => {
+    const likedBook = Object.assign(book, { likes: book.likes + 1 })
+    return dispatch => {
+      fetch(`http://localhost:3000/api/v1/books/${book.id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        method: 'PUT',
+        body: JSON.stringify(likedBook)
+      })
+      .then(response => response.json())
+      .then(book => dispatch(addLikesToBook(book)))
+      .catch(error => console.log(error))
+    }
+  }
