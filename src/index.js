b42c443994b3c203c2adec2816e80b4f1c1f7ebf
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-04-22 11:33:32 -0500

    install packages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'react-redux'
+import { Provider } from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
+
 
 ReactDOM.render(
   <React.StrictMode>
