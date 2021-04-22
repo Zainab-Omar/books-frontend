8c5f599fe434ef4a7685b5e1d5681fdf70f5de0c
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-04-22 13:23:10 -0500

    build bookReducer function

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'react-thunk'
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import {BrowserRouter as Router} from 'react-router-dom'
-import manageReducer from './reducers/manageReducer'
+import rootReducer from './reducers/manageReducers'
+import App from './App';
+
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(manageReducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 
 ReactDOM.render(
   <Provider store = {store}>
     <App />
   </Provider>
-  ,document.getElementById('root')
+   ,document.getElementById('root')
 )
 
---
+++ src/reducers/manageReducers.js
@@ -0,0 +1,54 @@
+import { combineReducers } from 'redux'
+
+// const rootReducer = combineReducers({
+//     books: bookReducer,
+//     comments: commentReducer
+// })
+
+// export default rootReducer;
+
+function bookReducer(state = [], action) {
+    switch (action.type){
+        // fetch all existing books in backend server
+        case 'FETCH_BOOKS':
+            return {books: action.payload}
+
+        // add new book to database
+        case 'ADD_BOOK':
+            return {...state, books: [...state.books, action.payload]}
+
+        // edit existing book in database
+        case 'EDIT_BOOK':
+            let findBook = state.books.map(book => {
+                if (book.id === action.payload.id){
+                    return action.payload
+                }
+                else
+                {return book}
+            })
+            return {...state, books: findBook}
+
+
+        // add likes to a book
+        case 'LIKE_BOOK':
+            return state.books.map(book => {
+                if (book.id === action.book.id) {
+                    return action.book
+                }
+                else
+                return book
+            })
+
+        default:
+            return state
+
+    }
+}
+
+// function commentReducer(state = [], action){
+//     switch(action.type) {
+//         case 'ADD_COMMENT':
+            
+//     }
+
+// }
