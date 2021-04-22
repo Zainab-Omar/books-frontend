833204c1bf175fd177b6cb0447edf8ced41bf1ee
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-04-22 14:54:54 -0500

    create fetchBooks action

---
+++ src/actions/fetchBooks.js
@@ -0,0 +1,11 @@
+export function fetchBooks() {
+
+    return dispatch => {
+        fetch('http://localhost:3000/api/v1/books')
+        .then(response => response.json())
+        .then(books => dispatch({
+            type: 'FETCH_BOOKS', payload: books
+        }))
+        .catch(error => console.log(error))
+    }
+}
