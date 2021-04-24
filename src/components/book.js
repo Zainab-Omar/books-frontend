ab24c0ebdd6f28437bc4ab8c8594b4b7abd0aaad
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-04-24 11:17:58 -0500

    update Books component

---
+++ src/components/book.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import editBook from './editBook'
+
+// this component is to show a single book details and edit it if needed
+const Book = ({books}) => {
+
+    return(
+     <p>hello</p>
+    )
+
+}
+
+export default Book
--- src/components/books.js
+++ src/components/books.js
@@ -1,20 +1,14 @@
 import React from 'react'
 import {Route, Link} from 'react-router-dom'
-// import Book from './Book'
+import Book from './book'
 
 const Books = ({books}) => {
-    const renderBooks = books.map(book => 
-        <div key={book.id}>
-         <img src={book.img_url} alt={book.id} width="200" height="200" key={book.id}/>
-        <p>Title: {book.title}</p>
-        <p>Author: {book.author}</p>
-        </div>
+    const renderBooks = books.map(book => <Book key={book.id} book={book} />
   
     )
     return(
         <div>
           {renderBooks}
-          {/* <Book books={this.props.books} /> */}
         </div>
     )
 }
