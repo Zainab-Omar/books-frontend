d72b13c75294e98d000c54e22dea75e85a7aa210
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-06-08 15:16:30 -0500

    build form that accept user input and send it with fetch request

--- src/containers/BooksContainer.js
+++ src/containers/BooksContainer.js
@@ -11,48 +11,54 @@ class BooksContainer extends React.Component {
     state = {
         // hold search term
         searchTerm: '',
-        sortbook: []
+        // sortbook: []
     }
 
     componentDidMount(){
         this.props.fetchBooks()
     }
 
-    // findBook = (value) => {
-    //    const newList = this.props.books.filter(book => {
-    //        const lc = book.title.toLowerCase();
-    //        const valueLc = value.toLowerCase();
-    //        return lc.includes(valueLc)
-    //    })
-
-    //    this.setState({searchTerm: newList})
-  
-
-    // }
-    findBook = (value) => {
-        const newList = this.props.books.filter(book => {
-            const lc = book.title.toLowerCase();
-            const valueLc = value.toLowerCase();
-            return lc.includes(valueLc)
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
         })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.props.fetchBooks(this.state.searchTerm)
+        this.setState({searchTerm: ''})
+    }
  
-        this.setState({searchTerm: newList})
+    // findBook = (value) => {
+    //     const newList = this.props.books.filter(book => {
+    //         const lc = book.title.toLowerCase();
+    //         const valueLc = value.toLowerCase();
+    //         return lc.includes(valueLc)
+    //     })
+ 
+    //     this.setState({searchTerm: newList})
    
  
-     }
+    //  }
 
-    sortBooks = () => {
-        const newList = this.props.books.sort((a,b) => a.title > b.title ? 1: -1)
-        this.setState({sortbooks: newList})
-    }
+    // sortBooks = () => {
+    //     const newList = this.props.books.sort((a,b) => a.title > b.title ? 1: -1)
+    //     this.setState({sortbooks: newList})
+    // }
 
     render(){
     //    console.log(this.props.books)
         return(
             <div>
                 <br />
-                <button onClick={this.sortBooks}>sort A-Z</button>
-                <SearchBook findBook = {this.findBook}/> 
+                {/* <button onClick={this.sortBooks}>sort A-Z</button> */}
+                {/* <SearchBook findBook = {this.findBook}/>  */}
+                <form onSubmit={this.handleSubmit}>
+                    <label>Search a Book: </label>
+                    <input type="text" name="searchTerm" value={this.state.searchTerm} onChange={this.handleChange}/>
+                    <input type="submit" />
+                </form>
                 <br />
                 <Books books = {this.state.searchTerm.length > 0 ? this.state.searchTerm : this.props.books} />
             </div>
