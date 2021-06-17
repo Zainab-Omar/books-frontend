2d2886a7c86312ebb4e76def548a3dc67a8aa7bd
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-06-17 15:10:16 -0500

    change conditional render in profile component to handle undefined users that I get with intial render  and undefined local storage when user not signin and redirect user to signin page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,22 @@
 import React from "react"
 import {connect} from 'react-redux';
+import { Redirect } from "react-router";
 
 const Profile = ({users})  => {
-    // console.log(users)
-    if (users === undefined){
-        return (null)
+
+    if(localStorage.token !== undefined){
+        if (users !== undefined){
+            return(
+                <div>
+               <img src={users.avatar} alt="picture" width="200"  height="200" />  
+                <h1>Welcome {users.username}</h1>
+                <p>bio: {users.bio}</p>
+                </div>
+            )
+        }
+        else return null
     }
-    return(
-        <div>
-       <img src={users.avatar} alt="picture" width="200"  height="200" />  
-        <h1>Welcome {users.username}</h1>
-        <p>bio: {users.bio}</p>
-        </div>
-    )
+    else return <Redirect to="/signin" />
 }
 
 const mapStateToProps = state => {
