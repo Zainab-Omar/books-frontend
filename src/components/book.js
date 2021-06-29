b61ff5df6dc70288193aad9052a3778076de0dd4
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-06-29 17:40:11 -0500

    add styling to book component

--- src/components/book.js
+++ src/components/book.js
@@ -6,15 +6,16 @@ import { Redirect } from "react-router";
 const Book = ({book, users}) => {
       const authors = book.volumeInfo.authors || []
     return(
-         <div>
-            <img src={book.volumeInfo.imageLinks === undefined ? "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/832px-No-Image-Placeholder.svg.png" : `${book.volumeInfo.imageLinks.thumbnail}`} width="200" height="200" alt="book image" />
+         <div className="book">
+            <div className="book-content">
+            <img className="book-image" src={book.volumeInfo.imageLinks === undefined ? "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/832px-No-Image-Placeholder.svg.png" : `${book.volumeInfo.imageLinks.thumbnail}`} width="300" height="400" alt="book image" />
             <p>Title: {book.volumeInfo.title}</p>
             <p>Authors: {authors.map(auther => auther)}</p>
             <p>publishedDate: {book.volumeInfo.publishedDate}</p>
             <a href= {book.saleInfo.buyLink === undefined ? `${book.volumeInfo.infoLink}` : `${book.saleInfo.buyLink}`} target="_blank">Buy Link</a>
             
             {users !== undefined ?  <SaveBook book={book} user = {users.id} /> : null} 
-           
+            </div>
          </div>
         
     )
--- src/containers/BooksContainer.js
+++ src/containers/BooksContainer.js
@@ -29,43 +29,17 @@ class BooksContainer extends React.Component {
         this.props.fetchBooks(this.state.searchTerm)
         this.setState({searchTerm: ''})
     }
- 
-    // findBook = (value) => {
-    //     const newList = this.props.books.filter(book => {
-    //         const lc = book.title.toLowerCase();
-    //         const valueLc = value.toLowerCase();
-    //         return lc.includes(valueLc)
-    //     })
- 
-    //     this.setState({searchTerm: newList})
-   
- 
-    //  }
-
-    // findBook = value => {
-    //     this.setState({searchTerm: value})
-    //     this.props.fetchBooks(this.state.searchTerm)
-    // }
-
-    // sortBooks = () => {
-    //     const newList = this.props.books.sort((a,b) => a.title > b.title ? 1: -1)
-    //     this.setState({sortbooks: newList})
-    // }
 
     render(){
-       console.log(this.props.books)
         return(
-            <div>
+            <div className="books-container">
                 <br />
-                {/* <button onClick={this.sortBooks}>sort A-Z</button> */}
-                {/* <SearchBook findBook = {this.findBook}/>  */}
                 <form onSubmit={this.handleSubmit}>
-                    <label>Search a Book: </label>
+                    <label>Search a Book:</label>
                     <input type="text" name="searchTerm" value={this.state.searchTerm} onChange={this.handleChange}/>
                     <input type="submit" />
                 </form>
                 <br />
-                {/* <Books books = {this.state.searchTerm.length > 0 ? this.state.searchTerm : this.props.books} /> */}
                 <Books books = {this.props.books} />
             </div>
         )
