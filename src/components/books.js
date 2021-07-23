d22f19c74e259e64f773e013ec6870a11392d59e
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-07-23 17:39:33 -0500

    update Books component

--- src/components/books.js
+++ src/components/books.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Book from './Book'
 
 const Books = ({books}) => {
-    const booksArray = books.items || []
+    const booksArray = books || []
     const renderBooks = booksArray.map(book => <Book key={book.id} book={book} />)
   return(
       <div>
