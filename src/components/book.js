a73eb5437ff5aa4ed0ea912b9623ca02ca230c9b
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-10-12 18:57:28 -0500

    change book component style

--- src/components/book.js
+++ src/components/book.js
@@ -2,20 +2,24 @@ import React from 'react'
 import SaveBook from './SaveBook'
 import {connect} from 'react-redux';
 import { Redirect } from "react-router";
-
+import { Card } from 'react-bootstrap'
 const Book = ({book, users}) => {
       const authors = book.volumeInfo.authors || []
     return(
          <div className="book">
-            <div className="book-content">
-            <img className="book-image" src={book.volumeInfo.imageLinks === undefined ? "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/832px-No-Image-Placeholder.svg.png" : `${book.volumeInfo.imageLinks.thumbnail}`} width="300" height="400" alt="book image" />
-            <p>Title: {book.volumeInfo.title}</p>
-            <p>Authors: {authors.map(auther => auther)}</p>
-            <p>publishedDate: {book.volumeInfo.publishedDate}</p>
-            <a href= {book.saleInfo.buyLink === undefined ? `${book.volumeInfo.infoLink}` : `${book.saleInfo.buyLink}`} target="_blank">Buy Link</a>
-            <br/>
-            {localStorage.token !== undefined ?  <SaveBook book={book} user = {users.id} /> : null} 
-            </div>
+            <Card style={{ width: '18rem', height: '550px' }}>
+               <Card.Img className="image-size" variant="top" src={book.volumeInfo.imageLinks === undefined ? "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/832px-No-Image-Placeholder.svg.png" : `${book.volumeInfo.imageLinks.thumbnail}`} />
+               <Card.Body>
+                  <Card.Title>Title: {book.volumeInfo.title}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">Authors: {authors.map(auther => auther)}</Card.Subtitle>
+                  <Card.Text>
+                      publishedDate: {book.volumeInfo.publishedDate}
+                  </Card.Text>
+                  <a href= {book.saleInfo.buyLink === undefined ? `${book.volumeInfo.infoLink}` : `${book.saleInfo.buyLink}`} target="_blank">Buy Link</a><br/>
+                  <Card.Link>{localStorage.token !== undefined ?  <SaveBook book={book} user = {users.id} /> : null} </Card.Link>
+               </Card.Body>
+         </Card>
+        
          </div>
         
     )
@@ -31,4 +35,12 @@ const mapStateToProps = state => {
 export default connect(mapStateToProps)(Book)
 
 
-
+{/* <div className="book-content">
+<img className="book-image" src={book.volumeInfo.imageLinks === undefined ? "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/832px-No-Image-Placeholder.svg.png" : `${book.volumeInfo.imageLinks.thumbnail}`} width="300" height="400" alt="book image" />
+<p>Title: {book.volumeInfo.title}</p>
+<p>Authors: {authors.map(auther => auther)}</p>
+<p>publishedDate: {book.volumeInfo.publishedDate}</p>
+<a href= {book.saleInfo.buyLink === undefined ? `${book.volumeInfo.infoLink}` : `${book.saleInfo.buyLink}`} target="_blank">Buy Link</a>
+<br/>
+{localStorage.token !== undefined ?  <SaveBook book={book} user = {users.id} /> : null} 
+</div> */}
