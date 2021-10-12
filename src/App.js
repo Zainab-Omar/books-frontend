51c8939d02fbe1d0447b5ac6086b25009c57a613
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-10-12 18:17:52 -0500

    change navbar style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import NavBar from './components/NavBar';
